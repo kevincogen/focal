f22c8930a0225d7d49b7d29d85d4a1f4aa7f3b95
Kevin Cogen <k.cogen@gmail.com>
2023-02-05 15:28:32 -0500

    refactor using .forEach to replace the for loop.

--- functions/callbacks-intro/findWaldo.js
+++ functions/callbacks-intro/findWaldo.js
@@ -1,8 +1,26 @@
 const findWaldo = function(names, found) {
+  names.forEach(function(value, index) {
+    if (value === "Waldo") {
+      found(index);
+    }
+  })
+};
+
+const actionWhenFound = function(index) {
+    console.log("Found him at " + index + "!");
+};
+
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound)
+
+
+// First excercise with for loop
+/*
+const findWaldo = function(names, found) {
   for (let i = 0; i < names.length; i ++) {
     let name = names[i];
     if (name === "Waldo") {
-      found(); // execute callback //this "outputs" found as (i)?? 
+      found(i); // execute callback //this "outputs" found as (i)?? 
     }
   }
 };
@@ -10,5 +28,4 @@ const findWaldo = function(names, found) {
 const actionWhenFound = function(i) {
   console.log("Found him at " + i + "!");
 };
-
-findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound)
+*/
